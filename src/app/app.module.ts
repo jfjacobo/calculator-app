72380a9b744958907bf62e1b3291155d452aa134
Juan Francisco Jacobo Rodriguez <jjacoborodrigue1@ncstudents.niagaracollege.ca>
2024-07-16 11:18:44 -0400

    Creating result component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import{FormsModule} from '@angular/forms';
-import { FormComponent } from './form/form.component'
+import { FormComponent } from './form/form.component';
+import { ResultComponent } from './result/result.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
